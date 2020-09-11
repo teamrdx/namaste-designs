6de6113f8d601030ff322da0a6a5efdb4f28eefe
Biplav Dahal <bipzification@gmail.com>
2020-09-11 16:41:49 +0200

    added checkout page, checkout item component and updated cart to use  selectors from reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,6 +8,8 @@ import  { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
+import { createStructuredSelector } from 'reselect';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
@@ -22,8 +24,8 @@ const mapDispatchToProps = dispatch =>({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = state => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector ({
+    itemCount: selectCartItemsCount
 });
 
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,9 @@ import { ReactComponent as Logo} from '../../assets/ndl.svg';
 import {auth} from '../../firebase/firebase.utils';
 import  CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { createStructuredSelector } from 'reselect';
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './header.styles.scss';
 
@@ -42,8 +45,8 @@ const Header = ({currentUser, hidden}) => (
     </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({ 
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({ 
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 export default connect(mapStateToProps)(Header);
